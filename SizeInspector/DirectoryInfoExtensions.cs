e4cff27fa51ee7f5b4cd1620ba4fdf1e122c19f0
sukerberk1 <szymon.l3232@gmail.com>
2023-08-03 13:54:20 +0200

    Initial

---
+++ SizeInspector/DirectoryInfoExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SizeInspectorExtensionMethods;
+
+public static class DirectoryInfoExtensions
+{
+    /* Function meant to be called when approaching a folder without subdirectories - folder that contains only files.*/
+    public static long TotalFilesSize(this DirectoryInfo folder) => folder.EnumerateFiles().Sum(f => f.Length);
+}
---
+++ SizeInspector/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using SizeInspectorExtensionMethods;
+
+namespace SizeInspector;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Unknown location.");
+            Console.WriteLine("Usage: SizeInspector <location eg. C:/>");
+            return;
+        }
+        DirectoryInfo location = new(@args[0]);
+
+        long totalSizeBytes = 0;
+
+        try
+        {
+            Console.WriteLine("Running...");
+            CalculateSize(ref totalSizeBytes, location);
+            double totalSizeGB = totalSizeBytes;
+            totalSizeGB /= Math.Pow(1024, 3);
+            Console.WriteLine($"Given directory: {location.FullName}; Its Size: {Math.Round(totalSizeGB, 2)} GB");
+        }
+        catch(UnauthorizedAccessException)
+        {
+            Console.WriteLine("One of analyzed files denied access. Try to run the program as an administrator.");
+        }
+
+    }
+
+
+    static void CalculateSize(ref long size, DirectoryInfo location)
+    {
+        size += location.TotalFilesSize();
+        foreach (var directory in location.GetDirectories())
+        {
+            CalculateSize(ref size, directory);
+        }
+    }
+
+}
